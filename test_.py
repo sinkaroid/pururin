7b65194a2257cbef988ba09daad3be2de4aee535
d0g <bibsindra@gmail.com>
2022-03-10 19:55:23 +0700

    Improve testing

--- test_.py
+++ test_.py
@@ -1,3 +1,11 @@
 import pururin
 data = pururin.Client()
-print(data.search_by_most_popular("jeanne alter"))
+
+print(data.get_book(61119))
+print(data.get_random())
+print(data.get_random_with_query("tomoe gozen"))
+print(data.search_by_highest_rated("jeanne alter"))
+print(data.search_by_most_viewed("succubus"))
+print(data.search_by_newest("gangbang"))
+print(data.search_by_random("yuri"))
+print(data.search_by_title("futanari"))
