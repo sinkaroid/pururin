773b0f38a8d36f083e78930f34e4f40e2bf3e70f
d0g <bibsindra@gmail.com>
2022-03-11 15:15:30 +0700

    build: cleanup setup

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup
 
 version = ''
 with open('pururin/__init__.py') as f:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
 
 requirements = []
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
-
 
 
 if not version:
@@ -31,8 +31,25 @@ setup(
     url='https://github.com/sinkaroid/pururin',
     packages=['pururin'],
     license='MIT',
+    classifiers=[
+        "Framework :: AsyncIO",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+
+    ],
     description='Pururin unofficial API',
     include_package_data=True,
-    keywords = ['pururin', 'doujin', 'wrapper'],
+    keywords=['pururin', 'doujin', 'wrapper'],
     install_requires=requirements
 )
